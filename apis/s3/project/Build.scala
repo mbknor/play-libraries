b7ab834ca85fc9d0e6899e6c702bd6b1b7de182c
Morten Kjetland <mbk@kjetland.com>
2012-10-21 22:05:15 +0200

    Fixed to be able publish own fixed version

--- apis/s3/project/Build.scala
+++ apis/s3/project/Build.scala
@@ -5,7 +5,7 @@ import PlayProject._
 object ApplicationBuild extends Build {
 
   val appName = "api-s3"
-  val appVersion = "1.5.0"
+  val appVersion = "1.5.0-with-headers_3"
 
   val appDependencies = Seq(
     "nl.rhinofly" %% "api-aws-utils" % "1.3")
@@ -15,10 +15,12 @@ object ApplicationBuild extends Build {
     Some("Rhinofly Internal " + repo.capitalize + " Repository" at "http://maven-repository.rhinofly.net:8081/artifactory/libs-" + repo + "-local")
   }
 
+  val mbknorRepository = Resolver.ssh("my local mbknor repo", "localhost", "~/projects/mbknor/mbknor.github.com/m2repo/releases/")(Resolver.mavenStylePatterns)
+
   val main = PlayProject(appName, appVersion, appDependencies, mainLang = SCALA).settings(
     organization := "nl.rhinofly",
     resolvers += rhinoflyRepo("RELEASE").get,
-    publishTo <<= version(rhinoflyRepo),
+    publishTo := Some(mbknorRepository),
     credentials += Credentials(Path.userHome / ".ivy2" / ".credentials"))
 
 }
